ae4f944f268e9f19d28880d0a81b4def26ae7dc8
Khusan <khusan7700@gmail.com>
2024-01-30 04:41:14 +0500

    inquirer gacha

--- index.js
+++ index.js
@@ -26,14 +26,40 @@
 
 //-----------------3----------------------
 
-const fs = require('fs');
-const data = fs.readFileSync("./input.txt", "utf8");
-console.log(data);
+// const fs = require('fs');
+// const data = fs.readFileSync("./input.txt", "utf8");
+// console.log(data);
 
-console.log("**************");
+// console.log("**************");
 
 
-fs.writeFileSync('./input.txt', `${data} \n\t\t by bekzodAli`);
-const new_data = fs.readFileSync("./input.txt", "utf8");
-console.log(new_data);
+// fs.writeFileSync('./input.txt', `${data} \n\t\t by bekzodAli`);
+// const new_data = fs.readFileSync("./input.txt", "utf8");
+// console.log(new_data);
  
+
+//--------------------------External module----------------------------------------
+//--------------------------1-----------------------------
+// External modeuleni ishlatishdan oldin moment ustanovka qilish kk. (npm install moment --save)
+
+// const moment = require('moment');
+
+
+// setInterval(() => {
+//     const time = moment().format();
+//     console.log(`Hozir vaqt: ${time}`);
+// }, 5000);
+
+//--------------------------2-----------------------------
+/*npm install inquirer--> teminalni ichidan biror jovobni tanlash 
+yoki user input kiritish narsalar uchin ishlatiladi
+terminalda kiritgan jovobimiz vscodega borsin kabi vazifa
+*/
+
+const inquirer = require("inquirer");
+inquirer
+.prompt([{type: "input", name: "raqam", message: "raqamni kiriting?"}])
+.then((answer) => { 
+    console.log("Man kiritgan raqam:", answer.raqam);
+ })
+.catch((err) => console.log(err));
